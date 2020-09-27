38c4c68e46bccf0cd68e430911b140d0a9b8fa69
Alex Mijail Boulchouk <amboulchouk@outlook.com>
2020-09-27 20:30:10 -0300

    second commit (develop)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MensajesController } from './mensajes/mensajes.controller';
+import { MensajesService } from './mensajes/mensajes.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'nest',
+      password: 'app',
+      database: 'sendmeapp_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController, MensajesController],
+  providers: [AppService, MensajesService],
 })
 export class AppModule {}
